50459195721c65aa85b120719a2b1dce216904ed
Teslia Ivan <jabsoluty@gmail.com>
2025-09-15 18:01:23 +0200

    added loading .env variables

--- main/main/settings.py
+++ main/main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+# from .env import get_config # that is the thing which is helping to store .env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
