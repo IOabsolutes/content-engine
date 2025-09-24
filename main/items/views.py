7d6e23065906fbea7dc44aad76ae6728cb6c1175
Teslia Ivan <jabsoluty@gmail.com>
2025-09-24 19:12:47 +0200

    added update item view

--- main/items/views.py
+++ main/items/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -37,7 +40,7 @@ def item_list_view(request):
     if not hasattr(request, "active_project") or request.active_project is None:
         messages.error(request, "Please activate a project first.")
         return render(request, "projects/activate.html", {})
-    
+
     object_list = Items.objects.filter(project=request.active_project)  # type: ignore
     return render(request, "items/list.html", {"object_list": object_list})
 
@@ -48,6 +51,29 @@ def item_detail_view(request, id=None):
     if not hasattr(request, "active_project") or request.active_project is None:
         messages.error(request, "Please activate a project first.")
         return render(request, "projects/activate.html", {})
-    
-    instance = get_object_or_404(Items, id=id, project=request.active_project)  # type: ignore
+
+    instance: Items = get_object_or_404(Items, id=id, project=request.active_project)  # type: ignore
     return render(request, "items/detail.html", {"object": instance})
+
+
+@login_required
+def item_detail_update_view(request, id=None):
+    if not hasattr(request, "active_project") or request.active_project is None:
+        messages.error(request, "Please activate a project first.")
+        return render(request, "projects/activate.html", {})
+
+    data = request
+
+    instance: Items = get_object_or_404(Items, id=id, project=request.active_project)  # type: ignore
+    form = ItemsForm(data, instance=instance)
+    form = ItemsForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        item_obj = form.save(commit=False)
+        item_obj.last_modified_by = request.user
+        item_obj.save()
+        messages.success(request, f"Item '{item_obj.title}' updated successfully!")
+        return redirect("item_detail", id=item_obj.id)
+    else:
+        messages.error(request, "Error updating item.")
+
+
